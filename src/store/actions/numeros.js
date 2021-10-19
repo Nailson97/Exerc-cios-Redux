2972eff7b80e1ef9a1a43f4e6980deda7e8f3afe
Nailson97 <nailsonsousa97@gmail.com>
2021-10-19 09:47:29 -0300

    fim estudo redux

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,6 +1,8 @@
+import { NUM_MIN_ALTERADO, NUM_MAX_ALTERADO } from './actionsTypes'
+
 export function alterarNumeroMinimo(novoNumero) {
     return {
-        type: 'NUM_MIN_ALTERADO',
+        type: NUM_MIN_ALTERADO,
         payload: novoNumero
     }
 }
@@ -8,7 +10,7 @@ export function alterarNumeroMinimo(novoNumero) {
 
 export function alterarNumeroMaximo(novoNumero) {
     return {
-        type: 'NUM_MAX_ALTERADO',
+        type: NUM_MAX_ALTERADO,
         payload: novoNumero
     }
 }
